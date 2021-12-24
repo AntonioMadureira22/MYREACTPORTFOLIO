4a7834fd2df240fe4055cb9052e6a265616c1524
antonio madureira <antonio.madureira2222@gmail.com>
2021-12-24 00:03:35 -0500

    tryimg to link other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Top from "./components/top/top";
+import intro from "./components/intro/intro"
 
 function App() {
   return (
     <div className="app">
       <Top/>
+      \<div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Work/>
+      </div>
     </div>
   );
 }
