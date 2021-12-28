d55982371b06f6de7417449c4de16c09a2dda5eb
antonio madureira <antonio.madureira2222@gmail.com>
2021-12-27 23:38:15 -0500

    added tags and styling for left top bar with some info

--- src/components/top/top.jsx
+++ src/components/top/top.jsx
@@ -1,16 +1,25 @@
 import "./top.scss"
+import {Code, Email} from "@material-ui/icons"
 
 export default function Top() {
     return (
         <div className="top">
             <div className="wrapper">
                <div className="left">
-                <a href="#intro">meme</a>
-                </div> 
+                    <a href="#intro" className="logo">apex</a>
+                    <div className="itemContainer">
+                        <Code className="icon"/>
+                        <span>+1 203 605 0316</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Email className="icon"/>
+                        <span>antonio.madureira2222@gmail.com</span>
+                    </div>
+                </div>
                 <div className="right">
-
+                    
                 </div>
             </div>
         </div>
-    )
+    );
 }
