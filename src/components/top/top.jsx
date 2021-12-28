09ebcaffacb449c927c2904f2e19a1e233cae007
antonio madureira <antonio.madureira2222@gmail.com>
2021-12-27 22:45:25 -0500

    added more styling with fixing the snap scroll effect

--- src/components/top/top.jsx
+++ src/components/top/top.jsx
@@ -3,7 +3,14 @@ import "./top.scss"
 export default function Top() {
     return (
         <div className="top">
+            <div className="wrapper">
+               <div className="left">
+                <a href="#intro">meme</a>
+                </div> 
+                <div className="right">
 
+                </div>
+            </div>
         </div>
     )
 }
