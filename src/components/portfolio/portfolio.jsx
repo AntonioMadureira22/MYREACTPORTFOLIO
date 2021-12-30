a3ddbb58c33575e5e6502a9735c4a1c5725740c1
antonio madureira <antonio.madureira2222@gmail.com>
2021-12-29 22:48:49 -0500

    fixed styling of pages and added onClick feature

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,16 +1,50 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/portfolioList";
 import "./portfolio.scss"
 
 export default function Portfolio() {
+    const [selected,setSelected] = useState("featured")
+    const list = [
+        {
+        id: "featured",
+        title: "featured",
+    },
+    {
+        id: "web",
+        title: "Web App",
+    },
+    {
+        id: "mobile",
+        title: "Mobile App",
+    },
+    {
+        id:"content",
+        title:"Content",
+    },
+    ];
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li>IDMB</li>
-                <li>PepWeb</li>
-                <li>Power's Family BBQ Sauce</li>
-                <li>Code Quiz</li>
-                <li>Automated Readme Maker</li>
+               {list.map(item=> (
+                   <PortfolioList 
+                   title={item.title} 
+                   active={selected === item.id} 
+                   setSelected={setSelected} 
+                   id={item.id}
+                   />
+               ))}
             </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="images/blogging.jpg" alt="pizza picture" />
+                    <h3>PepWeb</h3>
+                </div>
+                <div className="item">
+                    <img src="images/com-givvyquiz.jpg" alt="code quiz" />
+                    <h3>Code Quiz</h3>
+                </div>
+            </div>
         </div>
     )
 }
