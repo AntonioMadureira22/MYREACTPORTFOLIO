a351f42948afb699a97f36b4854400ceb690c127
antonio madureira <antonio.madureira2222@gmail.com>
2021-12-29 23:41:39 -0500

    finished images for my work on portfolio section

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/portfolioList";
 import "./portfolio.scss"
+import { featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    contentPortfolio} from "../../data";
 
 export default function Portfolio() {
-    const [selected,setSelected] = useState("featured")
+    const [selected,setSelected] = useState("featured");
+    const [data,setData] = useState([]);
     const list = [
         {
         id: "featured",
@@ -22,6 +27,26 @@ export default function Portfolio() {
         title:"Content",
     },
     ];
+
+    useEffect(() =>{
+        switch(selected){
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+                case "web":
+                setData(webPortfolio);
+                break;
+                case "mobile":
+                setData(mobilePortfolio);
+                break;
+                case "content":
+                setData(contentPortfolio);
+                break;
+                default:
+                    setData(featuredPortfolio);
+
+        }
+    },[selected])
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
@@ -36,14 +61,12 @@ export default function Portfolio() {
                ))}
             </ul>
             <div className="container">
-                <div className="item">
-                    <img src="images/blogging.jpg" alt="pizza picture" />
-                    <h3>PepWeb</h3>
-                </div>
-                <div className="item">
-                    <img src="images/com-givvyquiz.jpg" alt="code quiz" />
-                    <h3>Code Quiz</h3>
-                </div>
+                {data.map(d=>(
+                    <div className="item">
+                        <img src={d.img} />
+                        <h3>{d.title}</h3>
+                    </div>
+                ))}
             </div>
         </div>
     )
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "PepWeb",
+        img:"images/pepweb.png"
+    },
+    {
+        id: 2,
+        title: "Code Quiz",
+        img: "images/com-givvyquiz.jpg"
+    },
+];
+
+export const webPortfolio = [
+    {
+        id:1,
+        title: "Power's Family BBQ Sauce",
+        img: "images/apple-touch-icon.png"
+    },
+    {
+        id: 2,
+        title: "Automated Readme Generator",
+        img: "images/node.jpg"
+    },
+    {
+        id: 3,
+        title: "Noteify",
+        image: "images/note.png"
+    }
+];
+
+export const mobilePortfolio = [
+    {
+
+    },
+];
+
+export const contentPortfolio = [
+    {
+        id: 1,
+        title: "Twitch.tv/BigMoneyTone",
+        img: "images/twitch.png"
+    }
+];
