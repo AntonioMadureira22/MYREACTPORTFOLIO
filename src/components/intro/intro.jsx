b3f8b027687b3ca8bcdd5f8d17b362883392d920
antonio madureira <antonio.madureira2222@gmail.com>
2021-12-29 21:02:56 -0500

    styled the intro page and added several animations with images

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,9 +1,36 @@
 import "./intro.scss"
+import { init } from "ityped"
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            backDelay: 1500,
+            backSpeed: 50,
+            showCursor: true,
+            strings: ['Software Engineer', "Content Creator" ],
+        });
+    
+    }, []);
     return (
         <div className= "Intro" id="intro">
-
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="images/antonio.jpg" alt="Antonio" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello My Name Is,</h2>
+                    <h1>Antonio Madureira</h1>
+                    <h3> Full-Stack Developer <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="images/down-arrow.png" alt="scroll"></img>
+                </a>
+            </div>
         </div>
     )
 }
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,7 +3,14 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li>IDMB</li>
+                <li>PepWeb</li>
+                <li>Power's Family BBQ Sauce</li>
+                <li>Code Quiz</li>
+                <li>Automated Readme Maker</li>
+            </ul>
         </div>
     )
 }
