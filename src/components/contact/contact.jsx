d1b6294d626c735ae91893ed4ffd9d53fb052b09
antonio madureira <antonio.madureira2222@gmail.com>
2022-01-04 21:45:49 -0500

    added contact page

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,24 @@
 import  "./contact.scss"
 
 export default function Contact() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className= "contact" id="contact">
-
+            <div className="left">
+                <img src="images/computer email.png" alt="email logo" />
+            </div>
+            <div className="right">
+                <h2>Contact Me</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/top/top.jsx
+++ src/components/top/top.jsx
@@ -1,28 +1,39 @@
 import "./top.scss"
-import {Code, Email, GitHub} from "@material-ui/icons"
+import { Code, Email, GitHub } from "@material-ui/icons"
+import { makeStyles } from "@material-ui/core";
 
-export default function Top({ menuOpen, setMenuOpen}) {
-    <GitHub onClick={event =>  window.location.href='https://github.com/AntonioMadureira22'} />
+const useStyles = makeStyles(theme => ({
+    clickableIcon: {
+        color: "green",
+        "&hover": {
+            color: "yellow",
+        },
+    },
+}));
+
+export default function Top({ menuOpen, setMenuOpen }) {
+    const classes = useStyles();
     return (
         <div className={"top " + (menuOpen && "active")}>
             <div className="wrapper">
-               <div className="left">
+                <div className="left">
                     <a href="#intro" className="logo">apex</a>
                     <div className="itemContainer">
-                        <Code className="icon"/>
+                        <Code onClick={event => window.open.href = 'https://github.com/AntonioMadureira22'}
+                            className={classes.clickableIcon} className="icon" />
                         <span>+1 203 605 0316</span>
                     </div>
                     <div className="itemContainer">
-                        <Email className="icon"/>
+                        <Email className="icon" />
                         <span>antonio.madureira2222@gmail.com</span>
                     </div>
                     <div className="itemContainer">
-                        <GitHub className="icon"/>
+                        <GitHub className="icon" />
                         <span>AntonioMadureira22</span>
                     </div>
                 </div>
                 <div className="right">
-                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
                         <span>className="line1"</span>
                         <span>className="line2"</span>
                         <span>className="line3"</span>
